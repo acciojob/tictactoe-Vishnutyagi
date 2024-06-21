fdbc6a4e8e54d737a2af2bc2af5544e0fa99035d
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-06-21 14:15:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 document.getElementById('submit').addEventListener('click', function() {
     const player1 = document.getElementById('player1').value;
     const player2 = document.getElementById('player2').value;
-
-    // if (player1 === '' || player2 === '') {
-    //     alert('Please enter names for both players');
-    //     return;
-    // }
 document.querySelector('.input-section').style.display = 'none';
     const gameSection = document.querySelector('.game-section');
     gameSection.style.display = 'block';
